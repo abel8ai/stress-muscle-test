a711d7ff1ba5ec6919f1426cc4787afd971bae1f
ivehime6006 <ivehime6006@gmail.com>
2022-06-14 23:11:21 +0700

    Corregir el tema oscuro de la apk

--- app/src/main/java/com/uci/entrenamiento_muscular_estabilizador/ui/view/MainActivity.kt
+++ app/src/main/java/com/uci/entrenamiento_muscular_estabilizador/ui/view/MainActivity.kt
@@ -51,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         practicantViewModel
         athleteViewModel
 
+
         binding.tvLeerMasNuestraHistoria.setOnClickListener {
             val intent = Intent(this, AboutActivity::class.java)
             startActivity(intent)
